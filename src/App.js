7e5d98b312b1e33911e44275fc6c817f94adc9f4
Carlos Zalpa <nedhel.90@gmail.com>
2023-10-22 13:24:44 -0700

    Tabbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { Tabbar } from "./components/Tabbar";
 function App() {
     return (
         <div className="bg-dark-100 min-h-screen flex flex-col justify-center items-center">
-            <Register />;
+            <Login />
+            <Tabbar />;
+            <Register />
+            <Register />
+            <Register />
+            <Register />
+            <Register />
         </div>
     );
 }
