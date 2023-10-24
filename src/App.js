2ddb111a5f5290c1683d6a84838220f0b5132f34
Carlos Zalpa <nedhel.90@gmail.com>
2023-10-23 17:20:58 -0700

    react router dom added and configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import { Landing } from "./components/Landing";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { MainContainer } from "./components/MainContainer";
+import { AppContainer } from "./components/AppContainer";
 import { Profile } from "./components/Profile";
+import { Home } from "./components/Home";
+import { Search } from "./components/Search";
+import { Chat } from "./components/Chat";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
-    return (
-        <div className="bg-dark-100 min-h-screen flex flex-col justify-center items-center">
-            <Profile />;
-        </div>
-    );
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <MainContainer />,
+            errorElement: <div>Not found</div>,
+            children: [
+                { index: true, element: <Landing /> },
+                {
+                    path: "login",
+                    element: <Login />,
+                },
+                {
+                    path: "register",
+                    element: <Register />,
+                },
+                {
+                    path: "wave-hi",
+                    element: <AppContainer />,
+                    children: [
+                        { index: true, element: <Home /> },
+                        {
+                            path: "profile",
+                            element: <Profile />,
+                        },
+                        {
+                            path: "search",
+                            element: <Search />,
+                        },
+                        {
+                            path: "chat",
+                            element: <Chat />,
+                        },
+                    ],
+                },
+            ],
+        },
+    ]);
+    return <RouterProvider router={router} />;
 }
 
 export default App;
